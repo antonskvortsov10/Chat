1a29552a28c3a0eaf40a5327f58859087aa20de2
Anton Skvortsov <anton.skvortsov.1996@yandex.ru>
2020-04-17 00:59:04 +0300

    Add IServiceChat

---
+++ Chat/IServiceChat.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Chat
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IServiceChat" в коде и файле конфигурации.
+    [ServiceContract(CallbackContract = typeof(IServerChatCallback))]
+    public interface IServiceChat
+    {
+        [OperationContract]
+        int Connect();
+
+        [OperationContract]
+        void Disconnect(int id);
+
+        [OperationContract(IsOneWay = true)]
+        void SendMsg(string msg);
+    }
+
+    public interface IServerChatCallback
+    {
+        [OperationContract]
+        void MsgCallback(string msg);
+    }
+}
